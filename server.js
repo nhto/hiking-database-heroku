783dd76aa02afa33bf0cc86fbe76aa73bf7016c0
To Nok Hang, Tommy <55172561+nhto@users.noreply.github.com>
2022-02-19 10:56:41 +0800

    2022-02-19 push
    search function completed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var helmet = require('helmet');
 var app = express();
+//var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/hikedata';
 
 app.set("view engine", "jade")
 app.set("views", "MyViews")
@@ -105,3 +107,18 @@ var server = app.listen(process.env.PORT ||3000, function () {
     console.log('Node.js web server at port 3000 is running..')
     console.log('visit http://127.0.0.1:3000 ')
 });
+/*
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/hikedata';
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log('database connected');
+    var dbase = db.db("hikedata");
+   dbase.collection("hikedata"). find({'difficulty': '3 stars'}.toArray(function(err,result) {
+        if (err) throw err;
+        console.log(result);
+		console.log(result[0].name);
+        db.close();
+    });
+});*/
+
